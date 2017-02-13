beef2167e85f1ef6d667cc4c6d198ed2946e25ce
sainyajay@gmail.com <sainyajay@gmail.com>
2017-02-13 22:45:56 +0530

    port change further

--- firstServer.py
+++ firstServer.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 def hello():
     return "Hello world!"
 
-if __name__ == "__main__":
-    pno = os.environ.get("PORT", 4567)
-    port = int(pno)
-    app.run(host='localhost', port=port)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+EOF
